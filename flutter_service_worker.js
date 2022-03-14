711e95fb50fc6b20271ee8a674941586f500bf58
Guilherme Lunetta <49534365+guishas@users.noreply.github.com>
2022-03-14 20:21:18 -0300

    melhorias
    - leaderboard até 15 jogadores
    - focus retorna pro editext depois de dar enter
    - easter egg

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -18,9 +18,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "827dc087b8d85dd5119efa593db3f71a",
-"/": "827dc087b8d85dd5119efa593db3f71a",
-"main.dart.js": "986c53886a79db545f7cc74fb6d53187",
+"index.html": "fc0af30df78af0395d4623183f70c767",
+"/": "fc0af30df78af0395d4623183f70c767",
+"main.dart.js": "090e2e0bca1a04c69c217987734c28e0",
 "manifest.json": "f226982d3fb83cbbac39f986d32b87bd",
 "version.json": "09df46e12f228857cda37188dcaa0a35"
 };
